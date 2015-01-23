de6353171753f97d41e1c9b89a5ecc5df413a1a5
Romain <bentz.romain@gmail.com>
2015-01-23 18:02:12 +0100

    Ajout de trucs

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ then
 else
     screen -X -S tweets quit
     screen -S tweets -d -m /usr/bin/python /home/betezed/tweetsapi/microTweet.py
+    date +"%y-%m-%d %T Restart" >> /home/betezed/tweetsapi/log.txt
 fi
